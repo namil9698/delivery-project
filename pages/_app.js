2af55bb8369e473d465eedac265ea703d218c449
namil9698 <spdlqjtkdydwnd@naver.com>
2021-03-10 18:35:58 +0900

    pages폴더 _app.js 파일 생성.

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const App = ({ Component }) => {
+  return <Component />
+}
+
+App.prototype = {
+  Component: PropTypes.elementType.isRequired,
+}
+
+export default App
