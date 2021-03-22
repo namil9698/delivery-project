96f6c016a70a1bac8a64dfac69b1e30484a5ee65
namil9698 <spdlqjtkdydwnd@naver.com>
2021-03-22 19:38:25 +0900

    시뮬레이션 case문 변수로 관리.

--- redux/reducers/simulation.js
+++ redux/reducers/simulation.js
@@ -2,49 +2,54 @@ export const initialState = {
   myFoodList: [],
 };
 
+export const CHANGE_MYFOODLIST = 'CHANGE_MYFOODLIST';
+export const DELETE_MYFOOD = 'DELETE_MYFOOD';
+export const ADD_MYFOOD = 'ADD_MYFOOD';
+export const SUB_MYFOOD = 'SUB_MYFOOD';
+
 export const chageMyFoodList = data => {
   return {
-    type: 'CHANGE_MYFOODLIST',
+    type: CHANGE_MYFOODLIST,
     data,
   };
 };
 
 export const deleteMyFood = data => {
   return {
-    type: 'DELETE_MYFOOD',
+    type: DELETE_MYFOOD,
     data,
   };
 };
 
 export const addMyFood = data => {
   return {
-    type: 'ADD_MYFOOD',
+    type: ADD_MYFOOD,
     data,
   };
 };
 
 export const subMyFood = data => {
   return {
-    type: 'SUB_MYFOOD',
+    type: SUB_MYFOOD,
     data,
   };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'CHANGE_MYFOODLIST':
+    case CHANGE_MYFOODLIST:
       return {
         ...state,
         myFoodList: [...state.myFoodList, { ...action.data, qty: 1 }],
       };
 
-    case 'DELETE_MYFOOD':
+    case DELETE_MYFOOD:
       return {
         ...state,
         myFoodList: state.myFoodList.filter(food => food.id !== action.data.id),
       };
 
-    case 'ADD_MYFOOD':
+    case ADD_MYFOOD:
       return {
         ...state,
         myFoodList: state.myFoodList.map(food =>
@@ -52,7 +57,7 @@ const reducer = (state = initialState, action) => {
         ),
       };
 
-    case 'SUB_MYFOOD':
+    case SUB_MYFOOD:
       return {
         ...state,
         myFoodList: state.myFoodList.map(food =>
