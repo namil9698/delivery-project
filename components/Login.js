6a7c2937b3c01c6bc9eb30bdfbaf75475c50730b
namil9698 <spdlqjtkdydwnd@naver.com>
2021-04-01 17:16:41 +0900

    Revert "Revert "css 작업.""
    This reverts commit 731b70523dafaaf96ee483d221dba3cb9d74a5f4.

--- components/Login.js
+++ components/Login.js
@@ -25,21 +25,19 @@ const Login = () => {
         <LoginButton onClick={onClickLogout}>로그아웃</LoginButton>
       ) : (
         <>
-          <LoginWrapper>
-            <LoginButton>
-              <Button>
-                <ButtonImg onClick={() => onClickLogin('google')} className="google">
-                  <Img src="/images/login_google.png" />
-                  <Text>구글로 로그인</Text>
-                </ButtonImg>
+          <LoginButton>
+            <Button>
+              <ButtonImg onClick={() => onClickLogin('google')} className="google">
+                <Img src="/images/login_google.png" />
+                <Text>구글로 로그인</Text>
+              </ButtonImg>
 
-                <ButtonImg onClick={() => onClickLogin('anonymous')} className="anonymous">
-                  <Img src="/images/login_anonymous.png" />
-                  <Text>비회원 로그인</Text>
-                </ButtonImg>
-              </Button>
-            </LoginButton>
-          </LoginWrapper>
+              <ButtonImg onClick={() => onClickLogin('anonymous')} className="anonymous">
+                <Img src="/images/login_anonymous.png" />
+                <Text>비회원 로그인</Text>
+              </ButtonImg>
+            </Button>
+          </LoginButton>
         </>
       )}
     </>
@@ -48,29 +46,24 @@ const Login = () => {
 
 export default Login;
 
-const LoginWrapper = styled.div`
-  overflow-x: hidden;
-`;
-
 const LoginButton = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
 const Button = styled.div`
-  margin: 0 auto;
-  width: 70vw;
+  width: 100%;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
 
-  @media all and (max-width: 1100px) {
+  @media all and (max-width: 1200px) {
     width: 100%;
-    height: 500px;
     flex-direction: column;
     justify-content: center;
   }
@@ -79,9 +72,9 @@ const Button = styled.div`
 const ButtonImg = styled.div`
   position: relative;
   cursor: pointer;
-  width: 30vw;
+  width: 550px;
 
-  height: calc(30vw * 0.75);
+  height: calc(550px * 0.75);
 
   border-radius: 20%;
   transition: all 0.3s ease;
@@ -101,11 +94,15 @@ const ButtonImg = styled.div`
     background: linear-gradient(to bottom, rgba(255, 0, 132, 1) 0%, rgba(255, 0, 132, 1) 100%);
   }
 
-  @media all and (max-width: 1100px) {
+  @media all and (max-width: 1200px) {
     border-radius: 0;
     width: 100%;
-    height: calc(100vw * 0.75);
+    height: 50vh;
     box-shadow: none;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
     &:hover {
       transform: none;
     }
@@ -117,9 +114,10 @@ const Img = styled.div`
   width: 100%;
   height: 100%;
 
-  @media all and (max-width: 1100px) {
-    background-size: contain;
-    background-position: 50% 50%;
+  @media all and (max-width: 1200px) {
+    width: 320px;
+    height: calc(320px * 0.75);
+    background-size: 100% 100%;
     background-repeat: no-repeat;
   }
 `;
@@ -131,12 +129,12 @@ const Text = styled.p`
   top: 50%;
   transform: translate(-50%, -50%);
   font-family: 'InkLipquid';
-  font-size: 5vw;
+  font-size: 100px;
   font-weight: 800;
   color: #fff;
   text-shadow: 5px 5px #000;
 
-  @media all and (max-width: 1100px) {
+  @media all and (max-width: 1200px) {
     font-size: 50px;
   }
 `;
